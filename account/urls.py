6d85cdc7d5080528d107690474abf106b8eeb5c3
Wax30d <shaxzodbek_sobitov@mail.ru>
2022-03-13 16:32:44 +0500

    Created password_change urls.

--- account/urls.py
+++ account/urls.py
@@ -6,6 +6,8 @@ urlpatterns = [
     # post views
     # path('login/', views.user_login, name='login'),
 
+    path('password_change/', auth_views.PasswordChangeView.as_view(), name='password_change'),
+    path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('', views.dashboard, name='dashboard'),
